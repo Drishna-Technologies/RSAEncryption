2b98f619362e46371479d24ed424fd2eb496c533
priyanshudaksh-projects <priyanshudaksh@yahoo.com>
2022-07-19 07:12:42 +0530

    added encryption

---
+++ src/main/java/com/drishnaTechnologies/RSAsecurity/Controller/encryptedText.java
@@ -0,0 +1,15 @@
+package com.drishnaTechnologies.RSAsecurity.Controller;
+
+public class encryptedText {
+
+	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
