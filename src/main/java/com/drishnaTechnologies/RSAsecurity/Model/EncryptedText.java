de110f88b59c188da1f42d1c398036f7d13e5632
priyanshudaksh-projects <priyanshudaksh@yahoo.com>
2022-09-16 18:30:47 +0530

    Encryption and Decryption completed

---
+++ src/main/java/com/drishnaTechnologies/RSAsecurity/Model/EncryptedText.java
@@ -0,0 +1,15 @@
+package com.drishnaTechnologies.RSAsecurity.Model;
+
+public class EncryptedText {
+
+	private String encryptedText;
+
+	public String getText() {
+		return encryptedText;
+	}
+
+	public void setText(String text) {
+		this.encryptedText = text;
+	}
+
+}
